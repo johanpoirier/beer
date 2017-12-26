54d74efc85da5286b04cafc60961444fce9fc789
Johan Poirier <johan.poirier@gmail.com>
2017-12-26 15:07:33 +0100

    Adds Babel to transpile js

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,21 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new CopyWebpackPlugin([
       {
