81baa09a9c6a271562432ffd84b824fad43a059b
Rémi Bauzac <rbauzac@free.fr>
2017-12-28 13:58:40 +0100

    Fix regular expression
    The epub hash function can return negative integers.
    It's really difficult to have numeric hash function which ensure a
    positive result.
    So we need to adapt regular expression to handle '-' case in the url.

--- app/sw/beer-service-worker.js
+++ app/sw/beer-service-worker.js
@@ -2,7 +2,7 @@ self.importScripts('/jszip.js');
 
 const config = {
   version: 'suzuki-5',
-  epubPattern: /___\/(\w+)\/(.*)$/,
+  epubPattern: /___\/([\-\w]+)\/(.*)$/,
   cachePattern: /\.(?:css|js|jpg|png|svg|ttf|woff|eot|otf|html|xhtml|mp3|m4a)$/,
   debug: true
 };
