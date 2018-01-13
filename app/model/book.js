01b70b7464ae4210e754cdce3efbec4dcead1888
Rémi Bauzac <rbauzac@free.fr>
2018-01-13 16:14:29 +0100

    Add orientations global config for the book
    - Add orientation getters
    - Add documentation about spread and orientation

--- app/model/book.js
+++ app/model/book.js
@@ -14,7 +14,8 @@ class Book {
     this._encryptionData = encryptionData;
 
     this._format = extractFormat(metadata);
-    this._spread = extractRenditionSpread(metadata);
+    this._spread = extractRenditionDefaultSpread(metadata);
+    this._orientation = extractRenditionDefaultOrientation(metadata);
   }
 
   getSpineItem(index) {
@@ -69,8 +70,75 @@ class Book {
 Book.FORMAT_REFLOWABLE = 'reflowable';
 Book.FORMAT_FIXED_LAYOUT = 'pre-paginated';
 
+/**
+ * Specifies the intended Reading System synthetic spread behavior
+ * The rendition:spread property may also be specified locally on the
+ * Package Document spine itemref element, and will, in this case,
+ * override the global value for the given spine item.
+ * (Refer to Specifying name-value pairs on the spine itemref element
+ * for syntactical rules.)
+ *
+ * for this Publication or spine item :
+ *   - One page on "none"
+ *   - else 2 pages "both"
+ *   - else 1 page on landscape if portrait is set else 2 pages
+ *   - else 1 page on portrait if landscape is set else 2 pages
+ *   - no behavior define set our own
+ */
+
+/**
+ * Reading Systems must not incorporate this spine item in a synthetic spread.
+ */
 Book.RENDITION_SPREAD_NONE = 'none';
+/**
+ * No explicit synthetic spread behavior is defined. Reading Systems may use
+ * synthetic spreads in specific or all device orientations as part of a
+ * display area utilization optimization process.
+ */
 Book.RENDITION_SPREAD_AUTO = 'auto';
+/**
+ * Reading Systems should incorporate this spine item in a synthetic spread
+ * regardless of device orientation.
+ */
+Book.RENDITION_SPREAD_BOTH = 'both';
+/*
+ * Reading Systems should incorporate this spine item in a synthetic spread
+ * only when the device is in portrait orientation.
+ */
+Book.RENDITION_SPREAD_PORTRAIT = 'portrait';
+/**
+ * Reading Systems should incorporate this spine item in a synthetic spread
+ * only when the device is in landscape orientation.
+ */
+Book.RENDITION_SPREAD_LANDSCAPE = 'landscape';
+
+
+
+/**
+ * Specifies which orientation(s) the Author intends for the given Publication
+ * or spine item to be rendered in.
+ * The rendition:orientation property may also be specified locally
+ * on the Package Document spine itemref element, and will, in this case,
+ * override the global value for the given spine item.
+ * (Refer to Specifying name-value pairs on the spine itemref element
+ * for syntactical rules specific to local specification.)
+ */   
+
+/**
+ * The given spine item is intended for landscape rendering.
+ */
+Book.RENDITION_ORIENTATION_LANDSCAPE = 'landscape';
+
+/**
+ * The given spine item is intended for portrait rendering.
+ */
+Book.RENDITION_ORIENTATION_PORTRAIT = 'portrait';
+
+/**
+ * The given spine item is not orientation constrained.
+ */
+Book.RENDITION_ORIENTATION_AUTO = 'auto';
+
 
 export default Book;
 
@@ -82,10 +150,18 @@ function extractFormat(metadata) {
   return Book.FORMAT_REFLOWABLE;
 }
 
-function extractRenditionSpread(metadata) {
+function extractRenditionDefaultSpread(metadata) {
   const metaRenditionSpread = metadata['meta'].find(data => data['_property'] === 'rendition:spread');
   if (metaRenditionSpread) {
     return metaRenditionSpread['__text'];
   }
   return Book.RENDITION_SPREAD_AUTO;
+}
+
+function extractRenditionDefaultOrientation(metadata) {
+  const metaRenditionSpread = metadata['meta'].find(data => data['_property'] === 'rendition:orientation');
+  if (metaRenditionSpread) {
+    return metaRenditionSpread['__text'];
+  }
+  return Book.RENDITION_ORIENTATION_AUTO;;
 }
